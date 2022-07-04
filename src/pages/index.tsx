528679b854eea6edd0774e88927bd0a236d43a28
Henri Fourie <henri@fusebox.co.za>
2022-07-04 23:13:18 +0200

    implement tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const hello = trpc.useQuery(['hello', { text: 'client' }]);
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-middle">
       <div>
         <h1 className="text-center text-xl">What is the roundest Pokimon?</h1>
+        <small>{hello.data?.greeting}</small>
       </div>
       <div className="container flex flex-row justify-center align-middle mx-auto gap-10 mt-10 max-w-2xl">
         <div className="bg-red-600 p-10 rounded-xl"></div>
