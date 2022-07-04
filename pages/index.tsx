6b909721a628e0b87e6e09f34c92bd7b833dcdd8
Henri Fourie <henri@fusebox.co.za>
2022-07-04 22:53:53 +0200

    basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className='container mx-auto'>
-      <h1 className='bg-red-600'>Hello World</h1>
-      
+    <div className="h-screen w-screen flex flex-col justify-center align-middle">
+      <div>
+        <h1 className="text-center text-xl">What is the roundest Pokimon?</h1>
+      </div>
+      <div className="container flex flex-row justify-center align-middle mx-auto gap-10 mt-10 max-w-2xl">
+        <div className="bg-red-600 p-10 rounded-xl"></div>
+        <div className="text-center flex flex-col justify-center">vs</div>
+        <div className="bg-red-600 p-10 rounded-xl"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
